88174365ce29604cffaa3368ff609d3c42657aed
Murad Awad <muradawad21@berkeley.edu>
2017-10-02 20:22:52 -0700

    Added marker for user location

--- index.android.js
+++ index.android.js
@@ -1,36 +1,96 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
- import React, { Component } from 'react';
+import React, { Component } from 'react';
  import {
    AppRegistry,
    StyleSheet,
    Text,
-   View
+   View,
+   Dimensions
  } from 'react-native';
  import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+ var {width, height} = Dimensions.get('window');
+ const Aspect_Ratio = width/height;
+const LATITUDE = 37.8715926;
+const LONGITUDE = -122.27274699999998;
+const LATITUDE_DELTA = 0.01;
+const LONGITUDE_DELTA = LATITUDE_DELTA*Aspect_Ratio;
+var CustomMap = React.createClass({
+  getInitialState() {
+    return {
+      region: {
+        latitude: LATITUDE,
+        longitude: LONGITUDE,
+        latitudeDelta: LATITUDE_DELTA,
+        longitudeDelta: LONGITUDE_DELTA
+      },
+      marker:{
+        latitude:LATITUDE,
+        longitude: LONGITUDE,
+      }
+    };
+  },
 
- export default class pingit extends Component {
-   render() {
-     return (
-       <MapView
-         provider = {PROVIDER_GOOGLE}
-         style = {styles.map}
-         initialRegion={{
-           latitude: 39.7392,
-           longitude: -104.9903,
-           latitudeDelta: 0.0922,
-           longitudeDelta: 0.0421,
-         }}
-         />
-     );
-   }
- }
+  componentDidMount: function() {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({
+          region: {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+            latitudeDelta: LATITUDE_DELTA,
+            longitudeDelta: LONGITUDE_DELTA
+          },
+          marker:{
+            latitude:position.coords.latitude,
+            longitude:position.coords.longitude,
+          }
+        });
+      },
+      (error) => alert(error.message),
+    );
 
- const styles = StyleSheet.create({
+    this.watchID = navigator.geolocation.watchPosition((position) => {
+      const newRegion = {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+        latitudeDelta: LATITUDE_DELTA,
+        longitudeDelta: LONGITUDE_DELTA
+      }
+
+      this.onRegionChange(newRegion);
+    });
+  },
+
+  componentWillUnmount: function() {
+    navigator.geolocation.clearWatch(this.watchID);
+  },
+
+  onRegionChange(region) {
+    this.setState({ region });
+  },
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <MapView
+          ref="map"
+          mapType="terrain"
+          style={styles.map}
+          region={this.state.region}
+          onRegionChange={this.onRegionChange}
+        >
+        <MapView.Marker
+            coordinate = {this.state.marker}>
+        <View style = {styles.radius}>
+        <View style = {styles.marker}/>
+        </View>
+          </MapView.Marker>
+        </MapView>
+
+      </View>
+    );
+  },
+});
+ var styles = StyleSheet.create({
    container: {
      position: 'absolute',
      top: 0,
@@ -47,5 +107,26 @@
      right: 0,
      bottom: 0,
    },
+   radius:{
+     height: 50,
+     width: 50,
+     borderRadius: 50/2,
+     overflow:'hidden',
+     backgroundColor:'rgba(0,122,255,0.1)',
+     borderWidth: 1,
+     borderColor:'rgba(0,112,255,0.3)',
+     alignItems:'center',
+     justifyContent:'center'
+   },
+   marker:{
+     height: 20,
+     width: 20,
+     borderWidth: 3,
+     borderColor: '#ffffff',
+     borderRadius: 20 / 2,
+     overflow: 'hidden',
+     backgroundColor:'#007AFF'
+
+   }
  });
-AppRegistry.registerComponent('pingit', () => pingit);
+AppRegistry.registerComponent('pingit',()=> CustomMap);
