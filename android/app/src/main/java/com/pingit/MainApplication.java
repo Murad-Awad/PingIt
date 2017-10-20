25cd15e6d9933f1ae2f916ff9b28f2f4bbb53ee5
Murad Awad <muradawad21@berkeley.edu>
2017-10-20 16:22:03 -0700

    linked google-places api

--- android/app/src/main/java/com/pingit/MainApplication.java
+++ android/app/src/main/java/com/pingit/MainApplication.java
@@ -3,6 +3,7 @@ package com.pingit;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.arttitude360.reactnative.rngoogleplaces.RNGooglePlacesPackage;
 import com.magus.fblogin.FacebookLoginPackage;
 import com.geektime.rnonesignalandroid.ReactNativeOneSignalPackage;
 import com.airbnb.android.react.maps.MapsPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNGooglePlacesPackage(),
             new FacebookLoginPackage(),
             new ReactNativeOneSignalPackage(),
             new MapsPackage()
